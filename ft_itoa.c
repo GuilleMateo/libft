9be3714abd9e5aa0541f05b46ec3c850ad34f5e8
PimousseKa <127440645+PimousseKa@users.noreply.github.com>
2023-03-15 17:43:33 +0100

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,50 +12,50 @@
 
 #include "libft.h"
 
-int		len(long nb)
+static int
+	ft_abs(int nbr)
 {
-	int		len;
-
-	len = 0;
-	if (nb < 0)
-	{
-		nb = nb * -1;
-		len++;
-	}
-	while (nb > 0)
-	{
-		nb = nb / 10;
-		len++;
-	}
-	return (len);
+	return ((nbr < 0) ? -nbr : nbr);
 }
 
-char	*ft_itoa(int nb)
+static void
+	ft_strrev(char *str)
 {
-	char *str;
-	long	n;
-	int		i;
+	size_t	length;
+	size_t	i;
+	char	tmp;
 
-	n = nb;
-	i = len(n);
-	if (!(str = (char*)malloc(sizeof(char) * (i + 1))))
+	length = ft_strlen(str);
+	i = 0;
+	while (i < length / 2)
+	{
+		tmp = str[i];
+		str[i] = str[length - i - 1];
+		str[length - i - 1] = tmp;
+		i++;
+	}
+}
+
+char
+	*ft_itoa(int n)
+{
+	char	*str;
+	int		is_neg;
+	size_t	length;
+
+	is_neg = (n < 0);
+	if (!(str = ft_calloc(11 + is_neg, sizeof(*str))))
 		return (NULL);
-	str[i--] = '\0';
 	if (n == 0)
+		str[0] = '0';
+	length = 0;
+	while (n != 0)
 	{
-		str[0] = 48;
-		return (str);
+		str[length++] = '0' + ft_abs(n % 10);
+		n = (n / 10);
 	}
-	if (n < 0)
-	{
-		str[0] = '-';
-		n = n * -1;
-	}
-	while (n > 0)
-	{
-		str[i] = 48 + (n % 10);
-		n = n / 10;
-		i--;
-	}
+	if (is_neg)
+		str[length] = '-';
+	ft_strrev(str);
 	return (str);
 }
